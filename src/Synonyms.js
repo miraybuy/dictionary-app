e17d2611b85d0ea9680a1aa8512019a2e90a84f7
miraybuy <miraybuyukkaray@gmail.com>
2021-11-16 14:57:30 +0100

    integrated the synonyms

---
+++ src/Synonyms.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Synonyms(props) {
+  if (props.synonyms) {
+    return (
+      <div className="Synonyms">
+        {" "}
+        {props.synonyms.map(function (synonym, index) {
+          return <span key={index}> {synonym}</span>;
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
