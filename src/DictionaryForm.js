48d8334779f93fb593ba3b989c13604ca7b4e8b9
miraybuy <miraybuyukkaray@gmail.com>
2021-12-02 14:26:17 +0300

    added pexels photo api

--- src/DictionaryForm.js
+++ src/DictionaryForm.js
@@ -1,14 +1,26 @@
 import React, { useState } from "react";
 import "./DictionaryForm.css";
 import Results from "./Results";
+import Photos from "./Photos";
 import axios from "axios";
 
 export default function DictionaryForm() {
   let [vocabulary, setVocabulary] = useState("coding");
   let [definition, setDefinition] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState([]);
+
+  function handleImages(response) {
+    setPhotos(response.data.photos);
+  }
+
   function handleResponse(response) {
     setDefinition(response.data[0]);
+    let apiUrl = `https://api.pexels.com/v1/search?query=${response.data[0].word}&per_page=9`;
+    let apiKey = "563492ad6f917000010000010e9c62e44909491aab67baad10ee95be";
+    axios
+      .get(apiUrl, { headers: { Authorization: `Bearer ${apiKey}` } })
+      .then(handleImages);
   }
 
   function search() {
@@ -50,6 +62,7 @@ export default function DictionaryForm() {
           </form>
         </section>
         <Results definition={definition} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
---
+++ src/Photos.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function Photos(props) {
+  if (props.photos.length) {
+    return (
+      <section className="Photos">
+        <div className="row">
+          {props.photos.map(function (photo, index) {
+            return (
+              <div className="col-4" key={index}>
+                <a
+                  href={photo.src.original}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src={photo.src.landscape}
+                    className="img-fluid"
+                    alt={photo.photographer}
+                  />
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    );
+  } else {
+    return null;
+  }
+}
